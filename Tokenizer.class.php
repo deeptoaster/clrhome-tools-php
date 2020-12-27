dfe185329c4a8e0e4845df91786a9351a9f0aa5f
Deep Toaster <deeptoaster@gmail.com>
2020-12-26 22:41:40 -0800

    set up automatic download from canonical Catalog

--- Tokenizer.class.php
+++ Tokenizer.class.php
@@ -1,6 +1,9 @@
 <?
 namespace ClrHome;
 
+// TODO: move from conf.xml to catalog.xml
+define('TOKENIZER_CATALOG_URL', 'https://clrhome.org/catalog/conf.xml');
+
 abstract class Enum {
   public static function getConstList() {
     static $const_list = null;
@@ -20,7 +23,9 @@ abstract class Enum {
 
   public static function validate($value) {
     if (!in_array($value, static::getConstList())) {
-      throw new \OutOfBoundsException;
+      throw new \InvalidArgumentException(
+        sprintf('Invalid value for %s: %s', static::class, $value)
+      );
     }
 
     return $value;
@@ -33,7 +38,34 @@ abstract class Series extends Enum {
 }
 
 class Tokenizer {
+  protected $catalog;
   protected $series = Series::TI83P;
+
+  public function __construct($catalog_file = __DIR__ . '/catalog.xml') {
+    if (!file_exists($catalog_file)) {
+      $catalog_handle = fopen(TOKENIZER_CATALOG_URL, 'r');
+
+      if ($catalog_handle === false) {
+        throw new UnexpectedValueException(
+          'Unable to download catalog file at ' . $catalog_handle
+        );
+      }
+
+      if (file_put_contents($catalog_file, $catalog_handle) === false) {
+        throw new UnexpectedValueException(
+          "Unable to write catalog file at $catalog_file"
+        );
+      }
+    }
+
+    $this->catalog = simplexml_load_file($catalog_file);
+
+    if ($this->catalog === false) {
+      throw new UnexpectedValueException(
+        "Unable to read catalog file at $catalog_file"
+      );
+    }
+  }
 
   public function getSeries() {
     return $this->series;
