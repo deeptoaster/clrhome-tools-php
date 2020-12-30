b7e3db304d510ab0d74a286144db8d708735cd5b
Deep Toaster <deeptoaster@gmail.com>
2020-12-29 22:49:19 -0800

    add token table building https://trello.com/c/MaxKJRNl

--- Tokenizer.class.php
+++ Tokenizer.class.php
@@ -37,8 +37,16 @@ abstract class Series extends Enum {
   const TI83P = '**TI83F*';
 }
 
+abstract class Language extends Enum {
+  const AXE = 'axe';
+  const BASIC = '';
+  const GRAMMER = 'grammer';
+}
+
 class Tokenizer {
   protected $catalog;
+  protected $inverseCatalog;
+  protected $language = Language::BASIC;
   protected $series = Series::TI83P;
 
   public function __construct($catalog_file = __DIR__ . '/catalog.xml') {
@@ -46,25 +54,83 @@ class Tokenizer {
       $catalog_handle = fopen(TOKENIZER_CATALOG_URL, 'r');
 
       if ($catalog_handle === false) {
-        throw new UnexpectedValueException(
+        throw new OutOfBoundsException(
           'Unable to download catalog file at ' . $catalog_handle
         );
       }
 
       if (file_put_contents($catalog_file, $catalog_handle) === false) {
-        throw new UnexpectedValueException(
+        throw new OutOfBoundsException(
           "Unable to write catalog file at $catalog_file"
         );
       }
     }
 
     $this->catalog = simplexml_load_file($catalog_file);
+    $namespaces = $this->catalog->getDocNamespaces();
 
     if ($this->catalog === false) {
-      throw new UnexpectedValueException(
+      throw new OutOfBoundsException(
         "Unable to read catalog file at $catalog_file"
       );
     }
+
+    $token_idx = 0;
+
+    foreach ($this->catalog->children() as $token) {
+      switch ($token->getName()) {
+        case 'table':
+          $subtoken_idx = 0;
+
+          foreach ($token->children() as $subtoken) {
+            $this->registerToken(
+              $namespaces,
+              $subtoken,
+              $token_idx,
+              $subtoken_idx
+            );
+
+            $subtoken_idx++;
+          }
+
+          break;
+        case 'token':
+          $this->registerToken(
+            $namespaces,
+            $token,
+            $token_idx
+          );
+
+          break;
+      }
+
+      $token_idx++;
+    }
+  }
+
+  public function registerToken(
+    $namespaces,
+    $token,
+    $token_idx,
+    $subtoken_idx = null
+  ) {
+    foreach ($namespaces as $prefix => $namespace) {
+      $id = $token->attributes($prefix !== '' ? $namespace : '')['id'];
+
+      if ($id !== null) {
+        $this->inverseCatalog[':' . $id] = chr($token_idx) . (
+          $subtoken_idx !== null ? chr($subtoken_idx) : ''
+        );
+      }
+    }
+  }
+
+  public function getLanguage() {
+    return $this->language;
+  }
+
+  public function setLanguage($language) {
+    $this->language = Language::validate($language);
   }
 
   public function getSeries() {
