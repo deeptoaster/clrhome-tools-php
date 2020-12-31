52ac1782fbbc4158b4cfcf1e0af21842234cd44b
Deep Toaster <deeptoaster@gmail.com>
2020-12-30 23:21:40 -0800

    implement reversible detokenization https://trello.com/c/MaxKJRNl

--- Program.class.php
+++ Program.class.php
@@ -20,12 +20,20 @@ class Program {
   private $catalog;
   private $inverseCatalog;
 
+  public function getBodyAsChars() {
+    return $this->detokenize($this->body);
+  }
+
   public function setBodyAsChars($chars) {
     $this->body = $this->tokenize($chars);
   }
 
   public function getBodyAsTokens() {
     return $this->body;
+  }
+
+  public function setBodyAsTokens($tokens) {
+    $this->body = $tokens;
   }
 
   public function getCatalogFile() {
@@ -54,6 +62,104 @@ class Program {
 
   public function setSeries($series) {
     $this->series = Series::validate($series);
+  }
+
+  private function detokenize($tokens) {
+    if (!isset($this->catalog)) {
+      $this->initializeCatalog();
+    }
+
+    $namespace = $this->language !== Language::BASIC
+      ? $this->catalog->getDocNamespaces()
+      : '';
+    $chars = '';
+    $line_chars = '';
+    $line_tokens = '';
+    $token_start = 0;
+
+    while ($token_start <= strlen($tokens)) {
+      if ($token_start < strlen($tokens)) {
+        $token_sequence = $tokens[$token_start];
+        $token = $this->catalog->children()[ord($token_sequence)];
+
+        if ($token->getName() === 'table') {
+          if ($token_start + 1 === strlen($tokens)) {
+            throw new \UnexpectedValueException(
+              'Missing second byte in two-byte token sequence'
+            );
+          }
+
+          $second_byte = $tokens[$token_start + 1];
+          $token_sequence .= $second_byte;
+          $token = $token->children()[ord($second_byte)];
+        }
+
+        if ($token === null || $token['id'] === null) {
+          throw new \UnexpectedValueException(
+            'Unrecognized token ' . strtoupper(bin2hex($token_sequence))
+          );
+        }
+
+        $token_chars = $token->attributes($namespace)['id'];
+        $token_chars = $token_chars !== null ? $token_chars : $token['id'];
+      }
+
+      if ($token_start === strlen($tokens) || (string)$token_chars === "\n") {
+        $yolo = str_replace('\\', '', $line_chars);
+
+        if ($this->tokenize($yolo) === $line_tokens) {
+          $chars .= $yolo;
+        } else {
+          $slash_position = strpos($line_chars, '\\');
+
+          while (true) {
+            $test_end = strpos($line_chars, '\\', $slash_position + 1);
+
+            if ($test_end === false) {
+              $chars .= substr($line_chars, 0, $slash_position);
+              break;
+            }
+
+            $test_chars = substr($line_chars, 0, $slash_position) . substr(
+              $line_chars,
+              $slash_position + 1,
+              $test_end - $slash_position - 1
+            );
+
+            $test_tokens = $this->tokenize($test_chars);
+
+            if (
+              $test_tokens === substr($line_tokens, 0, strlen($test_tokens))
+            ) {
+              $slash_position = $test_end - 1;
+              $line_chars = $test_chars . substr($line_chars, $test_end);
+            } else {
+              $group_chars = substr($line_chars, 0, $slash_position + 1);
+              $group_tokens = $this->tokenize($group_chars);
+              $chars .= $group_chars;
+              $line_chars = substr($line_chars, $slash_position + 1);
+              $line_tokens = substr($line_tokens, strlen($group_tokens));
+              $slash_position = $test_end - $slash_position - 1;
+            }
+          }
+        }
+
+        $line_chars = '';
+        $line_tokens = '';
+
+        if ($token_start !== strlen($tokens)) {
+          $chars .= "\n";
+        }
+
+        $token_start++;
+      } else {
+        $line_chars .= "$token_chars\\";
+        $line_tokens .= $token_sequence;
+        $token_start += strlen($token_sequence);
+      }
+    }
+
+    return $chars;
   }
 
   private function initializeCatalog() {
@@ -122,7 +228,9 @@ class Program {
     $subtoken_idx = null
   ) {
     foreach ($namespaces as $prefix => $namespace) {
-      $id = $token->attributes($prefix !== '' ? $namespace : '')['id'];
+      $id = $token->attributes(
+        $prefix !== Language::BASIC ? $namespace : ''
+      )['id'];
 
       if ($id !== null) {
         $this->inverseCatalog[":$id"] = chr($token_idx) . (
@@ -133,30 +241,32 @@ class Program {
   }
 
   private function tokenize($chars) {
-    if ($this->inverseCatalog === null) {
+    if (!isset($this->inverseCatalog)) {
       $this->initializeCatalog();
     }
 
     $tokens = '';
     $chars = str_replace("\xe2\x86\x92", '->', $chars);
-    $line_start = 0;
 
-    while ($line_start < strlen($chars)) {
-      $newline_position = strpos($chars, "\n", $line_start);
-      $slash_position = strpos($chars, "\\", $line_start);
+    for (
+      $group_start = 0;
+      $group_start < strlen($chars);
+      $group_start = $group_end + 1
+    ) {
+      $newline_position = strpos($chars, "\n", $group_start);
+      $slash_position = strpos($chars, "\\", $group_start);
 
-      $line_end = min(
+      $group_end = min(
         $newline_position !== false ? $newline_position : strlen($chars),
         $slash_position !== false ? $slash_position : strlen($chars)
       );
 
-      $line = substr($chars, $line_start, $line_end - $line_start);
       $token_stack = array();
-      $token_start = $line_start;
-      $line_tokens = array();
+      $token_start = $group_start;
+      $group_tokens = array();
       $visited = array();
 
-      while ($token_start !== $line_end) {
+      while ($token_start !== $group_end) {
         if (isset($token_stack[$token_start])) {
           if (count($token_stack[$token_start]) === 0) {
             if (empty($token_stack)) {
@@ -164,14 +274,14 @@ class Program {
             } else {
               $visited[$token_start] = true;
               array_pop($token_stack);
-              $token_start -= strlen(array_pop($line_tokens));
+              $token_start -= strlen(array_pop($group_tokens));
             }
           } else {
-            $token = array_shift($token_stack[$token_start]);
-            $token_end = $token_start + strlen($token);
+            $token_chars = array_shift($token_stack[$token_start]);
+            $token_end = $token_start + strlen($token_chars);
 
             if (!isset($visited[$token_end])) {
-              $line_tokens[] = $token;
+              $group_tokens[] = $token_chars;
               $token_start = $token_end;
             }
           }
@@ -179,32 +289,36 @@ class Program {
           $token_stack[$token_start] = array();
 
           for (
-            $token_length = min($line_end - $token_start, 14);
+            $token_length = min($group_end - $token_start, 14);
             $token_length > 0;
             $token_length--
           ) {
-            $token = substr($chars, $token_start, $token_length);
+            $token_chars = substr($chars, $token_start, $token_length);
 
-            if (isset($this->inverseCatalog[":$token"])) {
-              $token_stack[$token_start][] = $token;
+            if (isset($this->inverseCatalog[":$token_chars"])) {
+              $token_stack[$token_start][] = $token_chars;
             }
           }
         }
       }
 
-      if ($token_start === $line_start) {
-        throw new \UnexpectedValueException("Unable to parse token near $line");
+      if ($token_start === $group_start) {
+        throw new \UnexpectedValueException(
+          'Unable to parse token near ' . substr(
+            $chars,
+            $group_start,
+            $group_end - $group_start
+          )
+        );
       }
 
-      $tokens = array_reduce($line_tokens, function($tokens, $token) {
-        return $tokens . $this->inverseCatalog[":$token"];
+      $tokens = array_reduce($group_tokens, function($tokens, $token_chars) {
+        return $tokens . $this->inverseCatalog[":$token_chars"];
       }, $tokens);
 
-      if ($line_end < strlen($chars) && $chars[$line_end] === "\n") {
+      if ($group_end < strlen($chars) && $chars[$group_end] === "\n") {
         $tokens .= $this->inverseCatalog[":\n"];
       }
-
-      $line_start = $line_end + 1;
     }
 
     return $tokens;
